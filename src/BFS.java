744696c14c6b7dc70556328490db1235e0c08779
Abha Upadhyay <abha.1012@gmail.com>
2017-06-03 18:45:41 -0700

    Update BFS.java

--- src/BFS.java
+++ src/BFS.java
@@ -22,3 +22,34 @@
 			}
 		}	
 	}
+
+
+
+	public void BFSGRAPH(int root)
+	{
+		
+		Boolean[] visited = new Boolean[V];
+		
+		Queue<Integer> q = new LinkedList<Integer>();
+		visited[root] = true;
+		q.add(root);
+		
+		while(!q.isEmpty())
+		{
+			int top = q.poll();
+			System.out.print(top);
+			
+			Iterator<Integer> it = adj[root].listIterator();
+			while(it.hasNext())
+			{
+				int n = it.next();
+				if(!visited[n])
+				{
+					visited[n] = true;
+					q.add(n);
+				}
+			}
+			
+		}
+		
+	}
